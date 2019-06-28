2d2a79581cf6b970e4940b72ceec863e82579652
MyOnClikc <inchood@yandex.ru>
2019-06-28 22:26:42 +0300

    ВСЕ

---
+++ js/script.js
@@ -0,0 +1,45 @@
+		$(window).load(function(){
+		$(".before-after").twentytwenty({
+			before_label: 'Без синали ',
+			after_label: 'Со скинали'
+		});
+			$('.before-slider').slick({
+				draggable: false,
+				dots:true,
+				dotsClass:'before-slider__dots',
+				prevArrow: $('.arrow-left'),
+				nextArrow: $('.arrow-right')
+			});
+			$('.menu-button').on('click', function(){
+				$('.menu').toggleClass('menu_active')
+			});
+			// Настройка select 
+			$('.select').on('click', function(){
+				$('.select__dropdown').toggleClass('select__dropdown_open');
+			});
+
+			$('.select__option').on('click', function(){
+				var value = $('this').attr("data-value");
+				$('#select-type').val(value);
+				$('.select_checked').text(value);
+				// $('.select__dropdown').toggleClass('select__dropdown_open');
+			});
+			$('a[href^="#"]').click(function() {
+				var _href = $(this).attr('href');
+				$('html, body').animate({ scrollTop: $(_href).offset().top -100 + 'px' });
+				return false
+			});
+			$('input[type="tel"]').mask('+7 (999) 999 99 99');
+			// показывать карту когда до нее дошли
+			var reviews = $('.reviews');
+			var reviewsTop = reviews.offset().top;
+			$(window).bind('scroll', function() {
+				var windowTop = $(this).scrollTop();
+				if (windowTop > reviewsTop) {
+					$('#map').html('<script type="text/javascript" charset="utf-8" async src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4fc6ff6979c19cc0730e6dc3906875d3175d6a20550696c2da51b0025eee0550&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script >')
+					$(window).unbind('scroll');
+				}
+			} )
+		});
+
+		
